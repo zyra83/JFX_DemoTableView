f615b86e71fc8362f94be96cc858951531717d92
Mickaël Benoit <zyra83@hotmail.com>
2017-05-04 13:49:54 +0200

    Modification du presenter pour utiliser les properties

--- src/presenter/IhmPrincipalePresenter.java
+++ src/presenter/IhmPrincipalePresenter.java
@@ -2,6 +2,7 @@ package presenter;
 
 import java.time.LocalDate;
 
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,21 +12,23 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import lombok.extern.apachecommons.CommonsLog;
-import model.entities.Matiere;
+import view.model.MatiereObs;
 
 @CommonsLog
 public class IhmPrincipalePresenter {
 
 	@FXML
-	private TableView<Matiere> tvMatieres;
+	private TableView<MatiereObs> tvMatieres;
 	@FXML
-	private TableColumn<Matiere, String> tcLibelle;
+	private TableColumn<MatiereObs, String> tcLibelle;
 	@FXML
-	private TableColumn<Matiere, Integer> tcCoef;
+	private TableColumn<MatiereObs, Integer> tcCoef;
 	@FXML
-	private TableColumn<Matiere, LocalDate> tcDate;
+	private TableColumn<MatiereObs, LocalDate> tcDate;
 	@FXML
 	private TextField tfLibelle;
 	@FXML
@@ -36,7 +39,7 @@ public class IhmPrincipalePresenter {
 	/**
 	 * Liste observable : tient la vue au courant de ses ajouts/supression.
 	 */
-	private ObservableList<Matiere> lstMatieres = FXCollections.observableArrayList();
+	private ObservableList<MatiereObs> lstMatieres = FXCollections.observableArrayList();
 
 	public IhmPrincipalePresenter() {
 
@@ -51,22 +54,22 @@ public class IhmPrincipalePresenter {
 
 	@FXML
 	public void initialize() {
-		lstMatieres.add(new Matiere("Math", 1, LocalDate.now().plusDays(1)));
+		lstMatieres.add(new MatiereObs("Math", 1, LocalDate.now().plusDays(1)));
 		this.tvMatieres.setItems(lstMatieres);
-		this.tcLibelle.setCellValueFactory(new PropertyValueFactory<>("nom"));
-		this.tcCoef.setCellValueFactory(new PropertyValueFactory<>("coef"));
-		this.tcDate.setCellValueFactory(new PropertyValueFactory<>("date"));
-
+		this.tcLibelle.setCellValueFactory((param) -> param.getValue().getNomProperty() );
+		// asObject parceque integerProperty n'éhrite pas de ObservableValue<Integer> donc on encapsule
+		this.tcCoef.setCellValueFactory((param) -> param.getValue().getCoefProperty().asObject()  );
+		this.tcDate.setCellValueFactory((param) -> param.getValue().getDateProperty() );
 	}
 
 	public void espionner() {
-		Matiere slctMatiere = this.tvMatieres.getSelectionModel().getSelectedItem();
+		MatiereObs slctMatiere = this.tvMatieres.getSelectionModel().getSelectedItem();
 		log.info(String.format("Matiere sélectionnée : %s", slctMatiere));
 	}
 
 	public void ajouter() {
 		// TODO: ajouter à la liste observable
-		Matiere m = new Matiere(tfLibelle.getText(), spCoef.getValue(), dpDate.getValue());
+		MatiereObs m = new MatiereObs(tfLibelle.getText(), spCoef.getValue(), dpDate.getValue());
 		this.lstMatieres.add(m);
 		// TODO: demander à la façade métier de persister le nouvel objet
 		// this.fm.ajouterMatiere(m);
@@ -80,7 +83,7 @@ public class IhmPrincipalePresenter {
 	}
 
 	public void supprimer() {
-		Matiere m = tvMatieres.getSelectionModel().getSelectedItem();
+		MatiereObs m = tvMatieres.getSelectionModel().getSelectedItem();
 		Integer index = tvMatieres.getSelectionModel().getSelectedIndex();
 		this.lstMatieres.remove(m);
 		if (log.isInfoEnabled())
@@ -88,7 +91,7 @@ public class IhmPrincipalePresenter {
 	}
 
 	public void editerCoef() {
-		Matiere m = tvMatieres.getSelectionModel().getSelectedItem();
+		MatiereObs m = tvMatieres.getSelectionModel().getSelectedItem();
 		Integer index = tvMatieres.getSelectionModel().getSelectedIndex();
 		TextInputDialog tid = new TextInputDialog();
 		tid.showAndWait();
